d3e4bed04676c65394e30331ceb57e83b9421643
LiangYutao <sinothk@126.com>
2018-03-27 14:25:02 +0800

    初始化

---
+++ app/src/main/java/com/sinothk/im/MainApplication.java
@@ -0,0 +1,24 @@
+package com.sinothk.im;
+
+import android.app.Application;
+import android.os.Environment;
+
+
+
+/**
+ * <pre>
+ *  创建:  梁玉涛 2018/3/26/026 on 14:58
+ *  项目: IMFrameLib
+ *  描述:
+ *  更新:
+ * <pre>
+ */
+public class MainApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+
+
+    }
+}
---
+++ app/src/main/java/com/sinothk/im/config/IMConfig.java
@@ -0,0 +1,21 @@
+package com.sinothk.im.config;
+
+import android.content.Context;
+
+/**
+ * <pre>
+ *  创建:  梁玉涛 2018/3/26/026 on 15:12
+ *  项目: IMFrameLib
+ *  描述:
+ *  更新:
+ * <pre>
+ */
+public class IMConfig {
+
+    static String tag = "IMConfig.init()";
+
+    public static void init(Context mContext) {
+
+
+    }
+}
